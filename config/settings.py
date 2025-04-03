a97179a4b2c326333e420cc23f8158bb2a1e89f7
alperencubuk <alperencubuk@outlook.com>
2025-04-03 13:53:24 +0300

    add default storage

--- config/settings.py
+++ config/settings.py
@@ -147,6 +147,9 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
